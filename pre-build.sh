3d093fb41e226ed35a13154e3de85fc03de5b15d
prostovelikij <oobobb@yandex.kz>
2025-12-14 01:51:31 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -2,9 +2,6 @@
 #cd padavan-ng/trunk/user/nfqws
 #find . -maxdepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
 
-# Замена CONFIG_RALINK_RAM_SIZE
-sed -i 's/^CONFIG_RALINK_RAM_SIZE.*/CONFIG_RALINK_RAM_SIZE=512/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
-
 # Включение 8 МБ флеш: CONFIG_RT2880_FLASH_8M=y и отключение 16 МБ
 sed -i 's/^# CONFIG_RT2880_FLASH_8M is not set/CONFIG_RT2880_FLASH_8M=y/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
 sed -i 's/^CONFIG_RT2880_FLASH_16M=.*/# CONFIG_RT2880_FLASH_16M is not set/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
