44cdffe639a8712702b150599fe06c6381bbfc50
prostovelikij <oobobb@yandex.kz>
2025-12-14 01:44:44 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -8,4 +8,4 @@ sed -i \
     padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
     
 # Замена CONFIG_MTD_STORE_PART_SIZ с 0x40000 на 0x20000
-sed -i 's/^CONFIG_MTD_STORE_PART_SIZ=.*/CONFIG_MTD_STORE_PART_SIZ=0x20000/' padavan-ng/trunk/configs/boards/XIAOMI/MI-MINI/kernel-3.4.x.config
+sed -i 's/^CONFIG_MTD_STORE_PART_SIZ=.*/CONFIG_MTD_STORE_PART_SIZ=0x20000/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
