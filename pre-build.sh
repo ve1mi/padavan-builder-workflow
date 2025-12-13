4885520fd742768146e33256b454567303319ce6
prostovelikij <oobobb@yandex.kz>
2025-12-13 22:46:15 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,3 +1,4 @@
 sed -i 's/^SRC_VER.*/SRC_VER = 72.3/' padavan-ng/trunk/user/nfqws/Makefile
 cd padavan-ng/trunk/user/nfqws
 find . -maxdepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
+cp "$GITHUB_WORKSPACE/my-kernel.config" "$GITHUB_WORKSPACE/padavan/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config"
