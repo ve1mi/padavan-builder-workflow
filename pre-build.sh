5583b1209833934a87a17dac09772f5b628bb639
prostovelikij <oobobb@yandex.kz>
2025-12-13 23:16:47 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,12 +1,4 @@
 #sed -i 's/^SRC_VER.*/SRC_VER = 72.3/' padavan-ng/trunk/user/nfqws/Makefile
 #cd padavan-ng/trunk/user/nfqws
 #find . -maxdepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
-cd padavan-ng/trunk/configs/boards/ASUS/RT-N14U
-cat > kernel-3.4.x.config << 'EOF'
-# Ваш полный kernel config здесь
-CONFIG_RT2880_FLASH_8M=y
-# CONFIG_RT2880_FLASH_16M is not set
-...остальное содержимое...
-EOF
-
-echo "[pre-build.sh] Kernel config written inline"
+cp ../my-kernel.config padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
