3ea964b9f694b237fac6d8593426c8554e67b347
prostovelikij <oobobb@yandex.kz>
2025-12-13 23:19:36 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,4 +1,4 @@
 #sed -i 's/^SRC_VER.*/SRC_VER = 72.3/' padavan-ng/trunk/user/nfqws/Makefile
 #cd padavan-ng/trunk/user/nfqws
 #find . -maxdepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
-cp ../my-kernel.config padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
+cp https://github.com/prostovelikij/padavan-builder-workflow/blob/main/my-kernel.config padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
