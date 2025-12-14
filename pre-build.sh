57113f659abdb34d84c48b9328aa7a6bf2013001
prostovelikij <oobobb@yandex.kz>
2025-12-14 13:55:19 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -9,13 +9,13 @@ sed -i 's/^# CONFIG_RT2880_FLASH_8M is not set$/CONFIG_RT2880_FLASH_8M=y/g' pada
 #sed -i 's/^CONFIG_RT2880_MTD_PHYSMAP_LEN=.*/CONFIG_RT2880_MTD_PHYSMAP_LEN=0x800000/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
 
 # Замена CONFIG_MTD_STORE_PART_SIZ с 0x40000 на 0x10000
-#sed -i 's/^CONFIG_MTD_STORE_PART_SIZ=.*/CONFIG_MTD_STORE_PART_SIZ=0x10000/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
+sed -i 's/^CONFIG_MTD_STORE_PART_SIZ=.*/CONFIG_MTD_STORE_PART_SIZ=0x20000/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
 
 #sed -i 's/pt_ralink_16m\.config/pt_ralink_8m_bigstor.config/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/partitions.config
 #sed -i 's/pt_ralink_16m.config/pt_ralink_8m_bigstor.config/g' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/partitions.config
 # Использование '!' в качестве разделителя вместо '/'
 
-sed -i 's!../../pt_ralink_16m.config!pt_ralink_8m_bigstor.config!g' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/partitions.config
+sed -i 's!../../pt_ralink_16m.config!pt_ralink_8m.config!g' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/partitions.config
 
 
 sed -i 's/^SRC_VER.*/SRC_VER = 72.4/' padavan-ng/trunk/user/nfqws/Makefile
