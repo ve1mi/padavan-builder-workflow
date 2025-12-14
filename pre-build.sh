22f839950ab55485ddb5d6cc58c57d682196df92
prostovelikij <oobobb@yandex.kz>
2025-12-14 03:02:19 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -5,6 +5,9 @@ sed -i 's/^CONFIG_RT2880_FLASH_16M=.*/# CONFIG_RT2880_FLASH_16M is not set/' pad
 # Замена CONFIG_RT2880_MTD_PHYSMAP_LEN с 0x1000000 на 0x800000
 sed -i 's/^CONFIG_RT2880_MTD_PHYSMAP_LEN=.*/CONFIG_RT2880_MTD_PHYSMAP_LEN=0x800000/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
 
+# Замена CONFIG_MTD_STORE_PART_SIZ с 0x40000 на 0x10000
+#sed -i 's/^CONFIG_MTD_STORE_PART_SIZ=.*/CONFIG_MTD_STORE_PART_SIZ=0x10000/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
+
 sed -i 's/^SRC_VER.*/SRC_VER = 72.3/' padavan-ng/trunk/user/nfqws/Makefile
 cd padavan-ng/trunk/user/nfqws
 find . -maxdepth 1 -mindepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
