2328f6ca9a7fea0d037e8bf087199906c414c03d
prostovelikij <oobobb@yandex.kz>
2025-12-14 03:29:18 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -6,7 +6,7 @@ sed -i 's/^CONFIG_RT2880_FLASH_16M=.*/# CONFIG_RT2880_FLASH_16M is not set/' pad
 sed -i 's/^CONFIG_RT2880_MTD_PHYSMAP_LEN=.*/CONFIG_RT2880_MTD_PHYSMAP_LEN=0x800000/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
 
 # Замена CONFIG_MTD_STORE_PART_SIZ с 0x40000 на 0x10000
-#sed -i 's/^CONFIG_MTD_STORE_PART_SIZ=.*/CONFIG_MTD_STORE_PART_SIZ=0x10000/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
+sed -i 's/^CONFIG_MTD_STORE_PART_SIZ=.*/CONFIG_MTD_STORE_PART_SIZ=0x10000/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
 
 sed -i 's/^SRC_VER.*/SRC_VER = 72.3/' padavan-ng/trunk/user/nfqws/Makefile
 cd padavan-ng/trunk/user/nfqws
