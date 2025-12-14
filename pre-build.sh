233b1594875b35ef0419c4ba42e088eb5e40a367
prostovelikij <oobobb@yandex.kz>
2025-12-14 04:33:55 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -8,7 +8,7 @@ sed -i 's/^CONFIG_RT2880_MTD_PHYSMAP_LEN=.*/CONFIG_RT2880_MTD_PHYSMAP_LEN=0x8000
 # Замена CONFIG_MTD_STORE_PART_SIZ с 0x40000 на 0x10000
 sed -i 's/^CONFIG_MTD_STORE_PART_SIZ=.*/CONFIG_MTD_STORE_PART_SIZ=0x10000/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/kernel-3.4.x.config
 
-echo "pt_ralink_8m.config" > padavan-ng/trunk/configs/boards/ASUS/RT-N14U/partitions.config
+sed -i 's/pt_ralink_16m\.config/pt_ralink_8m.config/' padavan-ng/trunk/configs/boards/ASUS/RT-N14U/partitions.config
 
 sed -i 's/^SRC_VER.*/SRC_VER = 72.3/' padavan-ng/trunk/user/nfqws/Makefile
 cd padavan-ng/trunk/user/nfqws
