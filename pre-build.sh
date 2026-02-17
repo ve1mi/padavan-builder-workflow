ba62915da7f6e108b364b3d5ce2631edd9506760
prostovelikij <oobobb@yandex.kz>
2026-02-17 13:40:28 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,17 +1,17 @@
-# 1. Получаем дату последнего коммита (ГГГГ.ММ.ДД) через заголовок или API
-# Если curl не сработает, поставим заглушку, чтобы сборка не упала с пустой строкой
+# 1. Получаем дату последнего коммита для версии
 LATEST_DATE=$(curl -s https://api.github.com | jq -r '.commit.committer.date' | cut -d'T' -f1 | tr -d '-')
-
-if [ -z "$LATEST_DATE" ]; then
-  echo "Ошибка: Не удалось получить дату. Используем текущую."
-  LATEST_DATE=$(date +%Y%m%d)
-fi
-
 LATEST_VER="72.$LATEST_DATE"
 
-# 2. Обновляем Makefile (используем двойные кавычки!)
-sed -i "s/^SRC_VER.*/SRC_VER = $LATEST_VER/" padavan-ng/trunk/user/nfqws/Makefile
+# 2. Путь к папке nfqws
+PKG_DIR="padavan-ng/trunk/user/nfqws"
 
-# 3. Очистка (как у вас)
-cd padavan-ng/trunk/user/nfqws
+# 3. Обновляем версию в Makefile
+sed -i "s/^SRC_VER.*/SRC_VER = $LATEST_VER/" "$PKG_DIR/Makefile"
+
+# 4. Очищаем папку от старых файлов (кроме Makefile и патчей)
+cd "$PKG_DIR"
 find . -maxdepth 1 -mindepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
+
+# 5. СКАЧИВАЕМ АКТУАЛЬНЫЙ MASTER И ПОДМЕНЯЕМ ИМ РЕЛИЗ
+# Мы скачиваем текущий срез кода и называем его так, как ждет Makefile
+curl -L "https://github.com/bol-van/zapret/archive/refs/heads/master.tar.gz" -o "zapret-$LATEST_VER.tar.gz"
