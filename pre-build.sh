cdcb91f76b640c6885a0fcb1c0fbf228dbd6b5ed
prostovelikij <oobobb@yandex.kz>
2026-02-17 14:36:31 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,12 +1,10 @@
-# 1. Получаем хэш последнего коммита (7 символов) через GitHub API
-LAST_HASH=$(curl -s https://api.github.com | grep sha | head -n 1 | cut -d '"' -f 4 | cut -c1-7)
+# 1. Меняем версию на master
+sed -i 's|^SRC_VER.*|SRC_VER = master|' padavan-ng/trunk/user/nfqws/Makefile
 
-# 2. Подставляем хэш в переменную SRC_VER (вместо фиксированного числа)
-sed -i "s/^SRC_VER.*/SRC_VER = $LAST_HASH/" padavan-ng/trunk/user/nfqws/Makefile
+# 2. Подменяем путь в URL: меняем /tags/v$(SRC_VER) на /heads/$(SRC_VER)
+# Используем | как разделитель, чтобы не экранировать слэши в URL
+sed -i 's|/tags/v$(SRC_VER)|/heads/$(SRC_VER)|' padavan-ng/trunk/user/nfqws/Makefile
 
-# 3. Убираем из URL привязку к тегам 'refs/tags/v', чтобы GitHub отдавал архив по хэшу
-sed -i 's|archive/refs/tags/v|archive/|g' padavan-ng/trunk/user/nfqws/Makefile
-
-# 4. Ваш оригинальный блок очистки
+# 3. Переходим в папку и очищаем всё, кроме Makefile и патчей
 cd padavan-ng/trunk/user/nfqws
 find . -maxdepth 1 -mindepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
