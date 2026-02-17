dad28f1cc2e206f8526c19e35a6ea45bded9b7b3
prostovelikij <oobobb@yandex.kz>
2026-02-17 13:22:50 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,11 +1,17 @@
-# 1. Получаем дату последнего коммита в формате ГГГГ.ММ.ДД (например, 72.20250216)
-# Используем ветку master как самую актуальную
-LAST_COMMIT_DATE=$(curl -s https://api.github.com | grep -oP '"date": "\K[^T]+' | tr -d '-')
-LATEST_VER="72.$LAST_COMMIT_DATE"
+# 1. Получаем дату последнего коммита (ГГГГ.ММ.ДД) через заголовок или API
+# Если curl не сработает, поставим заглушку, чтобы сборка не упала с пустой строкой
+LATEST_DATE=$(curl -s https://api.github.com | jq -r '.commit.committer.date' | cut -d'T' -f1 | tr -d '-')
 
-# 2. Обновляем Makefile
+if [ -z "$LATEST_DATE" ]; then
+  echo "Ошибка: Не удалось получить дату. Используем текущую."
+  LATEST_DATE=$(date +%Y%m%d)
+fi
+
+LATEST_VER="72.$LATEST_DATE"
+
+# 2. Обновляем Makefile (используем двойные кавычки!)
 sed -i "s/^SRC_VER.*/SRC_VER = $LATEST_VER/" padavan-ng/trunk/user/nfqws/Makefile
 
-# 3. Очистка и переход
+# 3. Очистка (как у вас)
 cd padavan-ng/trunk/user/nfqws
 find . -maxdepth 1 -mindepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
