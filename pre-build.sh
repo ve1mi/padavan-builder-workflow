d1a903bd47fd7c24a9c82487cdc1d421d6ff118b
prostovelikij <oobobb@yandex.kz>
2026-02-17 20:04:53 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,16 +1,16 @@
-# 1. Получаем актуальный короткий хэш из GitHub API
-# Это позволит заменить "master" на реальный номер (напр. 7a2b3c4)
+# 1. Получаем актуальный хэш мастер-ветки
 COMMIT_HASH=$(curl -s https://api.github.com | grep sha | head -n 1 | cut -d '"' -f 4 | cut -c1-7)
-
-# Если API недоступно, ставим заглушку, чтобы не сломать сборку
 [ -z "$COMMIT_HASH" ] && COMMIT_HASH="master"
 
-# 2. Устанавливаем версию (теперь здесь будет хэш вместо слова master)
-sed -i "s|^SRC_VER.*|SRC_VER = $COMMIT_HASH|" padavan-ng/trunk/user/nfqws/Makefile
+# 2. Устанавливаем версию master и правильный URL (через подмену SRC_VER)
+sed -i 's|^SRC_VER.*|SRC_VER = master|' padavan-ng/trunk/user/nfqws/Makefile
+sed -i 's|SRC_URL = .*|SRC_URL = https://github.com|' padavan-ng/trunk/user/nfqws/Makefile
 
-# 3. Подменяем URL (ссылка на master всегда отдает актуальный код)
-sed -i 's|SRC_URL = .*|SRC_URL = https://github.com/bol-van/zapret/archive/refs/heads/master.tar.gz|' padavan-ng/trunk/user/nfqws/Makefile
+# 3. ПОДМЕНЯЕМ ВЫВОД ВЕРСИИ: меняем макросы CFLAGS
+# Вместо v$(SRC_VER) подставляем vmaster, а вместо release — наш хэш
+sed -i "s|DZAPRET_GH_VER=v\$(SRC_VER)|DZAPRET_GH_VER=vmaster|g" padavan-ng/trunk/user/nfqws/Makefile
+sed -i "s|DZAPRET_GH_HASH=release|DZAPRET_GH_HASH=$COMMIT_HASH|g" padavan-ng/trunk/user/nfqws/Makefile
 
-# 4. Переходим и очищаем старые файлы
+# 4. Переходим и очищаем старые файлы (как в вашем исходном коде)
 cd padavan-ng/trunk/user/nfqws
 find . -maxdepth 1 -mindepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
