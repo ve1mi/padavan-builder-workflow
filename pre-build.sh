46f71fcb422755d4535f459b5e147f146e081d8b
prostovelikij <oobobb@yandex.kz>
2026-02-19 21:38:05 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,3 +1,2 @@
-sed -i 's/^SRC_VER.*/SRC_VER = 72.9/' padavan-ng/trunk/user/nfqws/Makefile
-cd padavan-ng/trunk/user/nfqws
-find . -maxdepth 1 -mindepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
+sed -i "s|^SRC_VER.*|SRC_VER = $(git ls-remote https://github.com/bol-van/zapret.git HEAD | cut -f1)|" padavan-ng/trunk/user/nfqws/Makefile
+cd padavan-ng/trunk/user/nfqws && find . -maxdepth 1 -mindepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
