827504ee118ab459994ced690645ee0f3a98e5ec
prostovelikij <oobobb@yandex.kz>
2026-02-19 21:11:47 +0300

    pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,39 +1,12 @@
-#!/bin/bash
+# 1. Устанавливаем версию (хэш)
+sed -i 's|^SRC_VER.*|SRC_VER = 53eba04|' padavan-ng/trunk/user/nfqws/Makefile
 
-# Настройки репозитория zapret
-REPO_OWNER="bol-van"
-REPO_NAME="zapret"
-MAKEFILE="padavan-ng/trunk/user/nfqws/Makefile"
+# 2. Меняем URL на архив конкретного коммита
+sed -i 's|SRC_URL = .*|SRC_URL = https://github.com|' padavan-ng/trunk/user/nfqws/Makefile
 
-echo "--- Настройка динамического обновления zapret (nfqws) ---"
+# 3. Важно: GitHub распакует это в папку "zapret-хэш", подправляем SRC_DIR
+sed -i 's|^SRC_DIR.*|SRC_DIR = zapret-53eba046ce1b6b8219bbefca6952dcc1c7bfb436|' padavan-ng/trunk/user/nfqws/Makefile
 
-# 1. Получаем хэш последнего коммита именно для этого репозитория
-# Добавлен конкретный путь к API репозитория
-LATEST_HASH=$(curl -s "https://api.github.com" | grep '"sha"' | head -n 1 | cut -d '"' -f 4 | cut -c 1-7)
-
-if [ -z "$LATEST_HASH" ]; then
-    echo "Ошибка: не удалось получить хэш. Используем заглушку."
-    LATEST_HASH="unknown"
-fi
-
-echo "Последний хэш: $LATEST_HASH"
-
-# 2. Очищаем старые исходники
-# Это важно, чтобы старые папки типа zapret-v1.x не мешали накладывать патчи на master
+# 4. Очистка старых исходников
 cd padavan-ng/trunk/user/nfqws
 find . -maxdepth 1 -mindepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
-cd - > /dev/null
-
-# 3. Модифицируем Makefile
-# SRC_URL теперь указывает на эндпоинт архивов GitHub
-# Makefile в Padavan обычно формирует ссылку как $(SRC_URL)/$(SRC_VER).tar.gz
-sed -i "s|^SRC_VER.*|SRC_VER = master|" $MAKEFILE
-sed -i "s|^SRC_URL = .*|SRC_URL = https://github.com|" $MAKEFILE
-
-# 4. Подставляем хэш в CFLAGS
-# Исправлено: экранирование и корректная подстановка переменной
-sed -i "s|CFLAGS += -DZAPRET_GH_VER=.*|CFLAGS += -DZAPRET_GH_VER=v\$(SRC_VER) -DZAPRET_GH_HASH=$LATEST_HASH|" $MAKEFILE
-
-echo "Makefile успешно обновлен: версия vmaster ($LATEST_HASH)"
-echo "URL загрузки: https://github.com/master.tar.gz"
-echo "--------------------------------------------------------"
